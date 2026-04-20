35962d70c2d07b62caf3cac1f5e696f688207779
Florian Wittemann <post@florian-zug.de>
2026-04-20 23:55:17 +0200

    switch to tls dns backend

--- flutter_app/lib/config/app_config.dart
+++ flutter_app/lib/config/app_config.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class AppConfig {
   static const String debugApiBase = 'http://localhost:8080/api/v1';
-  static const String releaseApiBase = 'http://192.168.178.5:8080/api/v1';
+  static const String releaseApiBase = 'https://iptv.florian-zug.de/api/v1';
 
   static bool get allowsCustomApiBase => kDebugMode;
 
